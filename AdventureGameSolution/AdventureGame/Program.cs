8389d3e4a5eab010e80e552e0d12eecdc7e84c65
M-Sheikha <mohammad.sheikha@outlook.com>
2020-09-24 16:36:37 +0200

    init

---
+++ AdventureGameSolution/AdventureGame/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace AdventureGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hej");
+        }
+    }
+}
