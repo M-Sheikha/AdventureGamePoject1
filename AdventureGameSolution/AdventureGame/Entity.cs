02ba42d03ab4b38f9f086db2b6c55b701768505b
David Ström <david.strom88@gmail.com>
2020-09-26 07:29:31 +0200

    Entity class

---
+++ AdventureGameSolution/AdventureGame/Entity.cs
@@ -0,0 +1,12 @@
+﻿namespace AdventureGame
+{
+    class Entity
+    {
+        public string Name { get; set; }
+        public string Race { get; set; }
+        public int Health { get; set; }
+        public int Damage { get; set; }
+        public int Strength { get; set; }
+        public int Constitution { get; set; }
+    }
+}
