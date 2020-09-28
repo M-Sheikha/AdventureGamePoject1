c3f30107f96dc758bbd65b156e4deb6d8640c6ae
DanielaBergdahl <dcode@rdqt.se>
2020-09-28 10:57:04 +0200

    Skapat GUI och topp av ram

---
+++ AdventureGameSolution/AdventureGame/GraphicalUserInterface.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AdventureGame
+{
+    class GraphicalUserInterface
+    {
+        public static int width = 100;
+
+        public static void PrintField()
+        {
+            //Skriver ut toppen av ramen
+            Console.Write("\n\t\u2554");
+            for (int i = 0; i < width - 2; i++)
+                Console.Write("\u2550");
+            Console.WriteLine("\u2557");
+            
+        }
+    }
+}
