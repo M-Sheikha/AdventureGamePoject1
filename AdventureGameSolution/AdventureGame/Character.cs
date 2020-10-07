8e36dc1b71c8339e97f59090e799c43eda24f747
David Ström <david.strom88@gmail.com>
2020-10-07 17:37:55 +0200

    Refactor

--- AdventureGameSolution/AdventureGame/Character.cs
+++ AdventureGameSolution/AdventureGame/Character.cs
@@ -96,7 +96,6 @@ namespace AdventureGame
             for (int i = 0; i < question.Length + name.Length; i++)
                 Console.Write(" ");
             Console.SetCursorPosition(left, top++);
-            //Console.Write(question + " ");
         }
 
 
@@ -110,8 +109,6 @@ namespace AdventureGame
             Console.WriteLine("[5] Dragonborn  [6] Gnome  [7] Half-Elf  [8] Half-Orc  [9] Tiefling");
             Console.SetCursorPosition(left, top++);
             var question = "Which race are you? ";
-            
-
 
             do
             {
@@ -160,180 +157,9 @@ namespace AdventureGame
                             return race;
 
                     TypeOver(question, "You have to enter an existing race.", playerChoice);
-
-                    //Console.CursorVisible = false;
-                    //Console.SetCursorPosition(left, top);
-                    //Console.WriteLine("You have to enter an existing race.");
-                    //Thread.Sleep(2000);
-                    //Console.SetCursorPosition(left, top);
-                    //for (int i = 0; i < 80; i++)
-                    //    Console.Write(" ");
-                    //Console.SetCursorPosition(left, --top);
-                    //for (int i = 0; i < 80; i++)
-                    //    Console.Write(" ");
-                    //Console.SetCursorPosition(left, top++);
-                    //Console.Write("Which race are you? ");
-
-                    //string race = TypeInRace(playerChoice);
-                    //if (race.Length > 2)
-                    //    return race;
-                    //Console.CursorVisible = false;
-                    //Console.SetCursorPosition(left, top);
-                    //Console.WriteLine("You have to enter an existing race.");
-                    //Thread.Sleep(2000);
-                    //Console.SetCursorPosition(left, top);
-                    //for (int i = 0; i < 80; i++)
-                    //    Console.Write(" ");
-                    //Console.SetCursorPosition(left, --top);
-                    //for (int i = 0; i < 80; i++)
-                    //    Console.Write(" ");
-                    //Console.SetCursorPosition(left, top++);
-                    //Console.Write("Which race are you? ");
                 }
 
-
-
-                //string playerChoice = Console.ReadLine();
-                //if (int.TryParse(playerChoice, out int choice))
-                //{
-                //    foreach (var race in Enum.GetValues(typeof(Races)))
-                //    {
-                //        if (true)
-                //    }
-
-                //    if (choice > 0 && choice < 10)
-                //    {
-                //        return choice switch
-                //        {
-                //            1 => "Dwarf",
-                //            2 => "Elf",
-                //            3 => "Hafling",
-                //            4 => "Human",
-                //            5 => "Dragonborn",
-                //            6 => "Gnome",
-                //            7 => "Half-Elf",
-                //            8 => "Half-Orc",
-                //            9 => "Tiefling",
-                //            _ => throw new NotImplementedException()
-                //        };
-                //    }
-                //    else
-                //    {
-                //        Console.SetCursorPosition(left, top);
-                //        Console.WriteLine("You have to choose between 1-9");
-                //        Thread.Sleep(2000);
-                //        Console.SetCursorPosition(left, top);
-                //        for (int i = 0; i < 80; i++)
-                //            Console.Write(" ");
-                //        Console.SetCursorPosition(left, --top);
-                //        for (int i = 0; i < 80; i++)
-                //            Console.Write(" ");
-                //        Console.SetCursorPosition(left, top++);
-                //        Console.Write("Which race are you? ");
-                //    }
-                //}
-                //else
-                //{
-                //    string race = TypeInRace(playerChoice.ToLower());
-                //    if (race.Length > 2)
-                //    {
-                //        return race;
-                //    }
-                //    else
-                //    {
-                //        Console.SetCursorPosition(left, top);
-                //        Console.WriteLine("You have to enter a valid command.");
-                //        Thread.Sleep(2000);
-                //        Console.SetCursorPosition(left, top);
-                //        for (int i = 0; i < 80; i++)
-                //            Console.Write(" ");
-                //        Console.SetCursorPosition(left, --top);
-                //        for (int i = 0; i < 80; i++)
-                //            Console.Write(" ");
-                //        Console.SetCursorPosition(left, top++);
-                //        Console.Write("Which race are you? ");
-                //    }
-                //}
-
             } while (true);
-        }
-        
-        private static string TypeInRace(string race)
-        {
-            switch (race)
-            {
-                case "d":
-                case "dw":
-                case "dwa":
-                case "dwar":
-                case "dwarf":
-                    return "Dwarf";
-                case "e":
-                case "el":
-                case "elf":
-                    return "Elf";
-                case "h":
-                case "ha":
-                case "hal":
-                case "half":
-                case "halfl":
-                case "halfli":
-                case "halflin":
-                case "halfling":
-                    return "Halfling";
-                case "hu":
-                case "hum":
-                case "huma":
-                case "human":
-                    return "Human";
-                case "dr":
-                case "dra":
-                case "drag":
-                case "drado":
-                case "dragon":
-                case "dragonb":
-                case "dragonbo":
-                case "dragonbor":
-                case "dragonborn":
-                    return "dragonborn";
-                case "g":
-                case "gn":
-                case "gno":
-                case "gnom":
-                case "gnome":
-                    return "Gnome";
-                case "half-e":
-                case "half-el":
-                case "half-elf":
-                case "half e":
-                case "half el":
-                case "half elf":
-                case "halfe":
-                case "halfel":
-                case "halfelf":
-                    return "Half-Elf";
-                case "half-o":
-                case "half-or":
-                case "half-orc":
-                case "half o":
-                case "half or":
-                case "half orc":
-                case "halfo":
-                case "halfor":
-                case "halforc":
-                    return "Half-Orc";
-                case "t":
-                case "ti":
-                case "tie":
-                case "tief":
-                case "tiefl":
-                case "tiefli":
-                case "tieflin":
-                case "tiefling":
-                    return "Tiefling";
-                default:
-                    return "";
-            }
         }
 
         private static string GetClass()
@@ -347,26 +173,14 @@ namespace AdventureGame
             Console.SetCursorPosition(left, top++);
             Console.WriteLine("[9] Rouge      [10] Sorcerer  [11] Warlock  [12] Wizard");
             Console.SetCursorPosition(left, top++);
-            Console.Write("What class are you? ");
+            var question = "What class are you? ";
             do
             {
+                Console.Write(question);
                 Console.CursorVisible = true;
                 string playerChoice = Console.ReadLine();
                 if (playerChoice.Equals(""))
-                {
-                    Console.CursorVisible = false;
-                    Console.SetCursorPosition(left, top);
-                    Console.WriteLine("You have to enter a class.");
-                    Thread.Sleep(2000);
-                    Console.SetCursorPosition(left, top);
-                    for (int i = 0; i < 80; i++)
-                        Console.Write(" ");
-                    Console.SetCursorPosition(left, --top);
-                    for (int i = 0; i < 80; i++)
-                        Console.Write(" ");
-                    Console.SetCursorPosition(left, top++);
-                    Console.Write("What class are you? ");
-                }
+                    TypeOver(question, "You have to enter a class.", playerChoice);
                 else if (int.TryParse(playerChoice, out int choice))
                 {
 
@@ -389,182 +203,18 @@ namespace AdventureGame
                             _ => throw new NotImplementedException()
                         };
                     }
-                    Console.CursorVisible = false;
-                    Console.SetCursorPosition(left, top);
-                    Console.WriteLine("You have to enter a number between 1-12.");
-                    Thread.Sleep(2000);
-                    Console.SetCursorPosition(left, top);
-                    for (int i = 0; i < 80; i++)
-                        Console.Write(" ");
-                    Console.SetCursorPosition(left, --top);
-                    for (int i = 0; i < 80; i++)
-                        Console.Write(" ");
-                    Console.SetCursorPosition(left, top++);
-                    Console.Write("What class are you? ");
+                    TypeOver(question, "You have to enter a number between 1-12.", playerChoice);
                 }
                 else
                 {
                     foreach (var _class in Enum.GetValues(typeof(Classes)))
                         if (_class.ToString().ToLower().Contains(playerChoice.ToLower()))
                             return _class.ToString();
-                    Console.CursorVisible = false;
-                    Console.SetCursorPosition(left, top);
-                    Console.WriteLine("You have to enter an existing class.");
-                    Thread.Sleep(2000);
-                    Console.SetCursorPosition(left, top);
-                    for (int i = 0; i < 80; i++)
-                        Console.Write(" ");
-                    Console.SetCursorPosition(left, --top);
-                    for (int i = 0; i < 80; i++)
-                        Console.Write(" ");
-                    Console.SetCursorPosition(left, top++);
-                    Console.Write("What class are you? ");
-                }
-                
-                //if (int.TryParse(playerChoice, out int choice))
-                //{
-                //    if (choice > 0 && choice < 14)
-                //    {
-                //        return choice switch
-                //        {
-                //            1 => "Barbarian",
-                //            2 => "Bard",
-                //            3 => "Cleric",
-                //            4 => "Druid",
-                //            5 => "Fighter",
-                //            6 => "Monk",
-                //            7 => "Paladin",
-                //            8 => "Ranger",
-                //            9 => "Rouge",
-                //            10 => "Sorcerer",
-                //            11 => "Warlock",
-                //            12 => "Wizard",
-                //            _ => throw new NotImplementedException()
-                //        };
-                //    }
-                //    else
-                //    {
-                //        Console.SetCursorPosition(left, top);
-                //        Console.WriteLine("You have to choose between 1-12");
-                //        Thread.Sleep(2000);
-                //        Console.SetCursorPosition(left, top);
-                //        for (int i = 0; i < 80; i++)
-                //            Console.Write(" ");
-                //        Console.SetCursorPosition(left, --top);
-                //        for (int i = 0; i < 80; i++)
-                //            Console.Write(" ");
-                //        Console.SetCursorPosition(left, top++);
-                //        Console.Write("What class are you? ");
 
-                //    }
-                //}
-                //else
-                //{
-                //    string _class = TypeInClass(playerChoice.ToLower());
-                //    if (_class.Length > 3)
-                //        return _class;
-                //    else
-                //    {
-                //        Console.SetCursorPosition(left, top);
-                //        Console.WriteLine("You have to enter a valid command");
-                //        Thread.Sleep(2000);
-                //        Console.SetCursorPosition(left, top);
-                //        for (int i = 0; i < 80; i++)
-                //            Console.Write(" ");
-                //        Console.SetCursorPosition(left, --top);
-                //        for (int i = 0; i < 80; i++)
-                //            Console.Write(" ");
-                //        Console.SetCursorPosition(left, top++);
-                //        Console.Write("What class are you? ");
-                //    }
-                //} 
+                    TypeOver(question, "You have to enter an existing class.", playerChoice);
+                }
             } while (true);
         }
-
-        //private static string TypeInClass(string _class)
-        //{
-        //    switch (_class)
-        //    {
-        //        case "barb":
-        //        case "barba":
-        //        case "barbar":
-        //        case "barbari":
-        //        case "barbaria":
-        //        case "barbarian":
-        //            return "Barbarian";
-        //        case "bard":
-        //            return "Bard";
-        //        case "c":
-        //        case "cl":
-        //        case "cle":
-        //        case "cler":
-        //        case "cleri":
-        //        case "cleric":
-        //            return "Cleric";
-        //        case "d":
-        //        case "dr":
-        //        case "dru":
-        //        case "drui":
-        //        case "druid":
-        //            return "Druid";
-        //        case "f":
-        //        case "fi":
-        //        case "fig":
-        //        case "figh":
-        //        case "fight":
-        //        case "fighte":
-        //        case "fighter":
-        //            return "Fighter";
-        //        case "m":
-        //        case "mo":
-        //        case "mon":
-        //        case "monk":
-        //            return "Monk";
-        //        case "p":
-        //        case "pa":
-        //        case "pal":
-        //        case "pala":
-        //        case "palad":
-        //        case "paladi":
-        //        case "paladin":
-        //            return "Paladin";
-        //        case "ra":
-        //        case "ran":
-        //        case "rang":
-        //        case "range":
-        //        case "ranger":
-        //            return "Ranger";
-        //        case "ro":
-        //        case "rou":
-        //        case "roug":
-        //        case "rouge":
-        //            return "Rouge";
-        //        case "s":
-        //        case "so":
-        //        case "sor":
-        //        case "sorc":
-        //        case "sorce":
-        //        case "sorcer":
-        //        case "sorcere":
-        //        case "sorcerer":
-        //            return "Sorcerer";
-        //        case "wa":
-        //        case "war":
-        //        case "warl":
-        //        case "warlo":
-        //        case "warloc":
-        //        case "warlock":
-        //            return "Warlock";
-        //        case "wi":
-        //        case "wiz":
-        //        case "wiza":
-        //        case "wizar":
-        //        case "wizard":
-        //            return "Wizard";
-        //        default:
-        //            return "";
-        //    }
-        //}
 
         private static void GetPlayerStats(Player player)
         {
